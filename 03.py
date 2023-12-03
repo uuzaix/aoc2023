bf28d566acd5706e9d6f9137d135bfe7c1df3fef
Larisa Dolgova <larisa.dolgova@gmail.com>
2023-12-03 20:23:16 +0100

    Reuse one fn since no multiple matches present

--- 03.py
+++ 03.py
@@ -18,39 +18,7 @@ def findSymbolsCoordinates(pattern, includeAll):
             symbols.append([row, symbolColumns  ])
     return symbols
 
-def getAdjacentNumbersToAnySymbol():
-    numbersArray = findNumbersCoordinates()
-    symbols = findSymbolsCoordinates('[^0-9.]', True)
-    result = 0    
-    for row in numbersArray:
-        numbersRow, numbers = row
-        if len(numbers) > 0:
-            for number in numbers:
-                startCol, endCol, value = number
-                match = 0
-                # check row above
-                if numbersRow-1 >= 0 and len(symbols[numbersRow-1][1]) > 0:
-                    for symCol in symbols[numbersRow-1][1]:
-                        if startCol-1 <=  symCol <=  endCol+1:
-                            match +=1
-                # check same row
-                if len(symbols[numbersRow][1]) > 0:
-                    for symCol in symbols[numbersRow][1]:
-                        if startCol-1 <=  symCol <=  endCol+1:
-                            match +=1
-                # check row below
-                if numbersRow+1 < len(symbols) and len(symbols[numbersRow+1][1]) > 0:
-                    for symCol in symbols[numbersRow+1][1]:
-                        if startCol-1 <=  symCol <=  endCol+1:
-                            match +=1
-                if match > 0:
-                    result += value 
-    return result
-
-
-def getAdjacentNumbersToStars():
-    numbersArray = findNumbersCoordinates()
-    symbolsArray = findSymbolsCoordinates('[*]', False)
+def getAdjacentNumbersToStars(numbersArray, symbolsArray, starsOnly):
     result = 0     
     for row in symbolsArray:
         symbolsRow, symbols = row
@@ -74,10 +42,16 @@ def getAdjacentNumbersToStars():
                     startCol, endCol, value = num
                     if startCol-1 <=  symbolCol <=  endCol+1:
                         matches.append(value)
-            if len(matches) == 2:
-               result += matches[0]*matches[1] 
+            if starsOnly :
+                if len(matches) == 2:
+                    result += matches[0]*matches[1] 
+            else:
+                result += sum(matches)
     return result
 
-print("Part 1:", getAdjacentNumbersToAnySymbol())
-print("Part 2:", getAdjacentNumbersToStars())
+numbersArray = findNumbersCoordinates()
+symbols = findSymbolsCoordinates('[^0-9.]', True)
+stars = findSymbolsCoordinates('[*]', False)
 
+print("Part 1:", getAdjacentNumbersToStars(numbersArray, symbols, False))
+print("Part 2:", getAdjacentNumbersToStars(numbersArray, stars, True))
