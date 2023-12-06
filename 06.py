eefd3cc7199cdf7b6f037c54625ca54a3ba77912
Larisa Dolgova <larisa.dolgova@gmail.com>
2023-12-06 20:51:01 +0100

    Day 6, part 2

--- 06.py
+++ 06.py
@@ -1,14 +1,26 @@
-time, dist = [x.split()[1:] for x in list(open('06.input'))]
 
-result = 1
 
-for i, x in enumerate(time):
-    ts = int(x)
+def part1():
+    time, dist = [x.split()[1:] for x in list(open('06.input'))]
+    result = 1
+    for i, x in enumerate(time):
+        ts = int(x)
+        count = 0
+        for t in range(1, ts):
+            if (t * (ts-t) > int(dist[i])):
+                count += 1
+        result = result * count
+    return result
+
+print("Part 1: ", part1())
+
+def part2():
+    time, dist = [int(x.split(":")[1].replace(" ", "")) for x in list(open('06.input'))]
     count = 0
-    for t in range(1, ts):
-        if (t * (ts-t) > int(dist[i])):
+    for t in range(1, time):
+        if (t * (time-t) > dist):
             count += 1
-    result = result * count
+    return count
 
-print(result)
 
+print("Part 2: ", part2())
