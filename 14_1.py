f6f89227955cbf688e64138e67fee11858659267
Larisa Dolgova <larisa.dolgova@gmail.com>
2023-12-14 13:11:19 +0100

    Day 14, part 1

---
+++ 14_1.py
@@ -0,0 +1,31 @@
+with open ('14.input') as f:
+    input = [x for x in f.read().split('\n')]
+
+board = [[letter for letter in line] for line in input]
+
+def tiltNorth(input):
+    board = input
+    # for line on the board
+    for ind, line in enumerate(board):
+        if ind!=0:
+            # for rock on the board
+            for i,s in enumerate(line):
+                if s == 'O':
+                    # for all rows above
+                    for j in [x for x in range(ind)][::-1]:
+                        if board[j][i] == '.':
+                            board[j][i] = 'O'
+                            board[j+1][i] = '.'
+                        else: 
+                            break
+    return board
+
+def count(board):
+    count = 0
+    for i, line in enumerate(tiltNorth(board)):
+        count += (len(board) - i)* line.count('O')
+    return count
+
+print("count", count(board))
+
+
